7e079d07108c1b451b97da670f8113a4e79b71e0
Mateusz Wygoda <mateusz.wygoda@gmail.com>
2022-02-17 23:50:58 +0100

    Add modification in Person.java and create employee.java class

---
+++ src/pl/workapp/model/Employee.java
@@ -0,0 +1,43 @@
+package pl.workapp.model;
+
+public class Employee extends Person{
+    private  double salary;
+    private String companyMail;
+    private String gender;
+
+    public Employee(String firstName, String lastName, double salary, String companyMail, String gender){
+        super(firstName, lastName);
+        this.salary = salary;
+        this.companyMail = companyMail;
+        this.gender = gender;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getCompanyMail() {
+        return companyMail;
+    }
+
+    public void setCompanyMail(String companyMail) {
+        this.companyMail = companyMail;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return getFirstName() +" "+getLastName()+ " salary: "+salary+" company mail: "+companyMail+" gender: "+gender;
+    }
+}
--- src/pl/workapp/model/Person.java
+++ src/pl/workapp/model/Person.java
@@ -3,14 +3,11 @@ package pl.workapp.model;
 public class Person {
     private String firstName;
     private String lastName;
-    private String privateMail;
-    private String privatePhone;
 
-    public Person(String firstName, String lastName, String privateMail, String privatePhone){
+
+    public Person(String firstName, String lastName){
         this.firstName = firstName;
         this.lastName = lastName;
-        this.privateMail = privateMail;
-        this.privatePhone = privatePhone;
     }
 
     public String getFirstName() {
@@ -29,19 +26,4 @@ public class Person {
         this.lastName = lastName;
     }
 
-    public String getPrivateMail() {
-        return privateMail;
-    }
-
-    public void setPrivateMail(String privateMail) {
-        this.privateMail = privateMail;
-    }
-
-    public String getPrivatePhone() {
-        return privatePhone;
-    }
-
-    public void setPrivatePhone(String privatePhone) {
-        this.privatePhone = privatePhone;
-    }
 }
