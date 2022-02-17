01e069c2f407b43897f20d08d6de89cb6d381010
Mateusz Wygoda <mateusz.wygoda@gmail.com>
2022-02-17 23:38:51 +0100

    Initial commit

---
+++ src/pl/workapp/app/WorkApp.java
@@ -0,0 +1,10 @@
+package pl.workapp.app;
+
+
+
+public class WorkApp {
+    private final static String  APP_VERSION = "Work app v 0.1";
+    public static void main(String[] args) {
+        System.out.println(APP_VERSION);
+    }
+}
---
+++ src/pl/workapp/app/WorkLogic.java
@@ -0,0 +1,4 @@
+package pl.workapp.app;
+
+public class WorkLogic {
+}
